f88b1a0f8807931977d350dad1ef8ca2adc992c9
Dr H <jmnabangi@gmail.com>
2025-03-06 18:19:39 +0300

    Removes livewire tables, updates blade files
    Signed-off-by: Dr H <jmnabangi@gmail.com>

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,4 @@
 <x-layouts.app title="Dashboard">
-    <livewire:churches-table />
 
     <x-maps-leaflet :centerPoint="['lat' => -1.28, 'long' => 36.82]" :zoomLevel="13" :smarkers="[[]]"></x-maps-leaflet>
 

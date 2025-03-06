e48a6db69d785649fd5fec290a11c050cd2c64df
Dr H <jmnabangi@gmail.com>
2025-03-06 16:24:10 +0300

    Fixes failing tests
    Signed-off-by: Dr H <jmnabangi@gmail.com>

--- resources/views/livewire/church-detail-modal.blade.php
+++ resources/views/livewire/church-detail-modal.blade.php
@@ -9,7 +9,7 @@
                     @foreach($church->masses as $mass)
 
                     <div class="flex flex-col pb-3">
-                        <dt class="mb-1 text-gray-500 md:text-lg dark:text-gray-400">{{$mass->day}} {{$mass->time}}</dt>
+                        <dt class="mb-1 text-gray-500 md:text-lg dark:text-gray-400">{{$mass->day}} {{$mass->formattedTime}}</dt>
                         <dd class="text-lg font-semibold">{{$mass->language}}</dd>
                     </div>
 
